952fed73be216c7bde6e28e3e3b6ee8ffc55bd44
WarpMiner <163392665+WarpMiner@users.noreply.github.com>
2025-01-16 19:26:01 +0700

    Create includes.h

---
+++ basedate/include/includes.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <unistd.h>
+#include <arpa/inet.h>
